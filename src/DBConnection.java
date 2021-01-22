49fab8346fa1531b4ea115fd137f527bf4ec3374
alejandrodk <ottoabarriosp@hotmail.com>
2021-01-22 17:28:55 -0300

    Create singleton db connection class

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,15 +3,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-
-    public Connection get_connection() {
-        Connection connect = null;
-        try {
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
-        } catch(SQLException error) {
-            System.out.println(error);
-        }
-        return connect;
+    private static Connection dbConnection;
+    public static Connection get_connection() {
+       if(dbConnection == null) {
+           try {
+               dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+           } catch(SQLException error) {
+               System.out.println(error);
+           }
+       }
+        return dbConnection;
     }
 
 }
