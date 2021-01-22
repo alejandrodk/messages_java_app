c8d41b7c466c5093e58ba2efca9f9849e2e895e0
alejandrodk <ottoabarriosp@hotmail.com>
2021-01-22 17:29:15 -0300

    Create a new message

--- src/MessageDAO.java
+++ src/MessageDAO.java
@@ -1,7 +1,25 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MessageDAO {
 
     public static void createMessage(Message message) {
-
+        try (Connection conn = DBConnection.get_connection()){
+            PreparedStatement ps = null;
+            try {
+                String query = "INSERT INTO mensajes (message, author) VALUES (?,?)";
+                ps = conn.prepareStatement(query);
+                ps.setString(1, message.getMessage());
+                ps.setString(2, message.getAuthor());
+                ps.executeUpdate();
+                System.out.println("Message created successfully");
+            } catch(SQLException exception) {
+                System.out.println(exception);
+            }
+        } catch(SQLException error){
+            System.out.println(error);
+        }
     }
 
     public static void readMessages() {
--- src/MessageService.java
+++ src/MessageService.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class MessageService {
 
     public static void createMessage() {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Type your message here");
+        String message = sc.nextLine();
+
+        System.out.println("Who are you?");
+        String author = sc.nextLine();
+
+        Message msg = new Message();
+        msg.setMessage(message);
+        msg.setAuthor(author);
+
+        MessageDAO.createMessage(msg);
     }
 
     public static void listMessages() {
